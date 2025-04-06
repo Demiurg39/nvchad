fb4517d8c665b8779b5b3121ab8a3635d2dcec4b
Demiurg39 <chiryagov2014@gmail.com>
2025-04-06 23:09:17 +0300

    source syntax cache

--- init.lua
+++ init.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
 }, lazy_config)
 
 -- load theme
+dofile(vim.g.base46_cache .. "syntax")
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
