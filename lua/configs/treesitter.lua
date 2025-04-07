00a3276d279f6539131377a54a06bd08b458504d
Demiurg39 <chiryagov2014@gmail.com>
2025-04-07 17:10:00 +0300

    refactor: tree-sitter opts moved to configs/

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,20 @@
+local opts = {
+  ensure_installed = {
+    "c",
+    "cpp",
+    "lua",
+    "java",
+    "json",
+    "make",
+    "markdown",
+    "nix",
+    "nu",
+    "regex",
+    "vim",
+    "vimdoc",
+  },
+
+  auto_install = true,
+}
+
+return opts
