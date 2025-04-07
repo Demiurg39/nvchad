7f864a51dac2399ee049e2e2814f64172772fa23
Demiurg39 <chiryagov2014@gmail.com>
2025-04-07 15:49:36 +0300

    formatters_by_ft extended

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,9 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    nix = { "alejandra" },
+    c = { "clang_format" },
+    cpp = { "clang_format" },
     _ = {
       "squeeze_blanks",
       "trim_whitespace",
@@ -8,9 +11,16 @@ local options = {
     },
   },
 
+  lsp_format = "fallback",
+
   formatters = {
     squeeze_blanks = {
       command = "cat",
+    },
+    clang_format = {
+      prepend_args = {
+        "-style={BasedOnStyle: LLVM, IndentWidth: 4}",
+      },
     },
   },
 }
