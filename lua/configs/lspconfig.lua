c186cf5d8a1bd231e3a904f8567c2bacec9bd0b6
Demiurg39 <chiryagov2014@gmail.com>
2025-04-07 15:52:41 +0300

    tweak: clangd lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,16 +5,42 @@ local nvlsp = require "nvchad.configs.lspconfig"
 local lspconfig = require "lspconfig"
 
 local servers = {
-  clangd = {},
+  clangd = {
+    extraOptions = {
+      capabilities = { offsetEncoding = { "utf-8" } },
+    },
+    init_options = {
+      usePlaceholders = true,
+      completeUnimported = true,
+      clangdFileStatus = true,
+    },
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--clang-tidy",
+      "--header-insertion=iwyu",
+      "--completion-style=detailed",
+      "--function-arg-placeholders",
+      "--fallback-style=llvm",
+    },
+  },
   jdtls = {},
   nil_ls = {},
 }
 
 -- lsps with default config
 for name, opts in pairs(servers) do
-   opts.on_attach = nvlsp.on_attach
-   opts.on_init = nvlsp.on_init
-   opts.capabilities = nvlsp.capabilities
+  opts.on_attach = nvlsp.on_attach
+  opts.on_init = nvlsp.on_init
+  opts.capabilities = nvlsp.capabilities
 
-    lspconfig[name].setup(opts)
+  lspconfig[name].setup(opts)
 end
+
+lspconfig.opts = {
+  servers = {
+    clangd = {
+      mason = false,
+    },
+  },
+}
