2db32ab02d2acf3a035b037b0fc517be035536db
Demiurg39 <chiryagov2014@gmail.com>
2025-04-09 11:04:55 +0300

    fix macros notify via noice

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -5,6 +5,12 @@ local opts = {
     signature = { enabled = false },
     progress = { enabled = false },
   },
+  routes = {
+    {
+      filter = { event = "msg_showmode" },
+      view = "cmdline",
+    },
+  },
 }
 
 return opts
