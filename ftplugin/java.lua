ea6ee0d9fb19e68de0c4026c4374d3855bb8a446
Demiurg39 <chiryagov2014@gmail.com>
2025-04-12 15:07:27 +0300

    fixup! setup for java
    using glob pattern for easier updates

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,7 +3,7 @@ local mason = vim.fn.stdpath "data" .. "/mason/packages"
 local jdtls = mason .. "/jdtls"
 local java_dap_bin = mason
   .. vim.fn.glob("/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
-local jar = jdtls .. "/plugins/org.eclipse.equinox.launcher_" .. "1.7.0.v20250331-1702" .. ".jar"
+local jar = jdtls .. "/plugins/org.eclipse.equinox.launcher_" .. "*.*.*.v*-*" .. ".jar"
 local jdtls_conf = jdtls .. "/config_linux"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = vim.fn.expand "$XDG_CACHE_HOME" .. "/jdtls/projects/" .. project_name
@@ -25,7 +25,7 @@ local config = {
     "java.base/java.lang=ALL-UNNAMED",
 
     "-jar",
-    jar,
+    vim.fn.glob(jar, true),
 
     "-configuration",
     jdtls_conf,
